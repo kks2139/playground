7553e88bb989abcd42152c5c9b758adb375277c7
kwangsun <eyeyel@naver.com>
2022-06-29 22:45:26 +0900

    prettierrc 추가

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,18 +1,20 @@
-import classNames from "classnames/bind";
+import classNames from 'classnames/bind';
 import styles from './Input.module.scss';
 
 const cx = classNames.bind(styles);
 
-function Input(){
-    return(
-        <div className={cx('Input', {shadow: true})}>
-            <input className={cx('test', 'back')}/>
-            <div className={cx('test')}>
-                <div className={cx('back')}>123</div>
-            </div>
-            <span className={cx('text')}>글자</span>
-        </div>
-    );
+function Input() {
+            return (
+                        <div className={cx('Input', {shadow: true})}>
+                                    <input className={cx('test', 'back')} />
+                                    <div className={cx('test')}>
+                                                <div className={cx('back')}>
+                                                            123
+                                                </div>
+                                    </div>
+                                    <span className={cx('text')}>글자</span>
+                        </div>
+            );
 }
 
 export default Input;
